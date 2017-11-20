b6df268efbedb29cb515086bf9060f454d76e35f
taisazero <taisazero@github.com>
2017-11-20 16:06:22 -0500

    first commit

---
+++ Grader.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import nltk
+import pandas as pd
+
+def browse():
+    from tkinter.filedialog import askopenfilename
+
+    Tk().withdraw()
+    filename = askopenfilename()
+    return filename
+
+print('Pick Gradebook')
+grades=browse()
+print('Pick grades to add')
+toAdd=browse()
+
+gradebook=pd.read_csv(grades)
+new_grades=pd.read_csv(toAdd)
+
+
